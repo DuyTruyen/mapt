4d15f243573d49965572e56de5dec08e65e30214
DuyTruyen <duytruyen.msc@gmail.com>
2022-04-21 17:33:34 +0700

    gg

---
+++ script.js
@@ -0,0 +1,57 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+  function (locationVn) {
+    const longitude = locationVn.coords.longitude;
+    const latitude = locationVn.coords.latitude;
+    const coords = [latitude, longitude];
+
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    // Click map
+
+    map.on('click', function (mapEvent) {
+      const { lat, lng } = mapEvent.latlng;
+      
+    //   L.marker([lat, lng]).addTo(map).L.popup({ maxWidth: 500, minWidth: 100 })
+        
+    //     .setContent('<p>Hello world!<br />This is a nice popup.</p>')
+    //     .openOn(map);
+
+        L.marker([lat, lng]).addTo(map).bindPopup(L.popup({ maxWidth: 500, minWidth: 100, autoClose: false,closeOnClick:false,className:'running-popup' }).setContent('<p>Hello world!<br />This is a nice popup.</p>')).openPopup();
+    });
+
+    L.marker(coords).addTo(map).bindPopup().openPopup();
+  },
+  function () {}
+);
+
+// .getCurrentPosition(
+//   function () {
+//     console.log(locationVn);
+
+//     const longitude = locationVn.longitude;
+//     const latitude = locationVn.latitude;
+//     console.log(`https://www.google.com/maps/@${longitude},${latitude},15z`);
+//   },
+//   function () {
+//     alert('Không thể truy cập vị trí');
+//   }
+// );
+// console.log(locationVn);
