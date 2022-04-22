67b08c608ab77ad0034caf34d3143f913af0c975
DuyTruyen <duytruyen.msc@gmail.com>
2022-04-22 15:12:44 +0700

    p3

--- script.js
+++ script.js
@@ -11,47 +11,91 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-navigator.geolocation.getCurrentPosition(
-  function (locationVn) {
+class App {
+#map;
+#mapEvent;
+
+
+
+  constructor() {
+    this._getPosition();
+  }
+  _getPosition() {
+    navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function () {
+      alert('Khong the truy cap vi tri');
+    });
+  }
+;
+  _loadMap(locationVn) {
+    console.log(this);
+    
+    console.log(locationVn);
+    
     const longitude = locationVn.coords.longitude;
     const latitude = locationVn.coords.latitude;
     const coords = [latitude, longitude];
 
-    const map = L.map('map').setView(coords, 13);
+    this.#map = L.map('map').setView(coords, 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+    }).addTo(this.#map);
 
-    // Click map
+    this.#map.on(
+      'click',
+      function (mapE) {
+        this.#mapEvent = mapE;
+  
+  
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      },
+      function () {}
+    );
 
-    map.on('click', function (mapEvent) {
-      const { lat, lng } = mapEvent.latlng;
+  }
+;
 
-    // Hiện input
-    
-    form.classList.remove('hidden');
-    
+  _showForm(){
 
-      L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(
-          L.popup({
-            maxWidth: 500,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup',
-          }).setContent('<p>Hello world!<br />This is a nice popup.</p>')
-        )
-        .openPopup();
-    });
-
-    L.marker(coords).addTo(map).bindPopup('Vị trí của bạn').openPopup();
-  },
-  function () {}
-);
+  }
+  _toggle(){
 
 
+  }
+}
 
+const app = new App();
+
+
+//   // Click map
+
+
+
+//   form.addEventListener('submit', function (e) {
+//     const { lat, lng } = mapEvent.latlng;
+//     // Xóa ô input
+//     inputDistance.value=inputDuration.value=inputElevation.value=inputCadence.value='';
+
+//     e.preventDefault();
+//     L.marker([lat, lng])
+//       .addTo(map)
+//       .bindPopup(
+//         L.popup({
+//           maxWidth: 500,
+//           minWidth: 100,
+//           autoClose: false,
+//           closeOnClick: false,
+//           className: 'running-popup',
+//         }).setContent('<p>Hello world!<br />This is a nice popup.</p>')
+//       )
+//       .openPopup();
+//   });
+// });
+
+// inputType.addEventListener('change',function(){
+//   inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+//   inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+
+// })
